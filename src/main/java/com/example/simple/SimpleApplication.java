7955a4da90d8659f50a07cc76f84542fc02c8cd8
Dengyang <dy1532615902@153.com>
2019-09-03 22:40:15 +0800

    modifie session

--- src/main/java/com/example/simple/SimpleApplication.java
+++ src/main/java/com/example/simple/SimpleApplication.java
@@ -7,9 +7,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
 import java.util.Map;
 
 
@@ -36,6 +39,7 @@ public class SimpleApplication extends SpringBootServletInitializer {
 	}
 
 	@GetMapping("/session")
+	@ResponseBody
 	public String getSession(HttpServletRequest httpServletRequest){
 		return httpServletRequest.getSession().getId();
 	}
